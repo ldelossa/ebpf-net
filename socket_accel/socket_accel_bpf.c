3282c69f09415b2dde5eb3c56f6b37483ccd50a8
ldelossa <louis.delos@gmail.com>
2023-02-27 03:36:37 +0000

    wip, building

--- socket_accel/socket_accel_bpf.c
+++ socket_accel/socket_accel_bpf.c
@@ -17,6 +17,12 @@
 #define TC_ACT_SHOT		2
 #define TC_ACT_REDIRECT		7
 
+#define IPV4_IHL_BYTES(ip4) \
+        ((ip4->ihl * 32) / 8)
+
+#define IPPROTO_UDP 17
+#define IPPROTO_TCP 6
+
 struct {
         __uint(type, BPF_MAP_TYPE_SOCKHASH);
         __uint(max_entries, 65535);
@@ -24,34 +30,61 @@ struct {
         __type(value, __u64);
 } accepted_sockets SEC(".maps");
 
-static __always_inline int skb_extract_tcp(struct __sk_buff *skb, uint32_t l4_off, struct bpf_sock_tuple *tuple) {
+static __always_inline int skb_extract_tcp(struct __sk_buff *skb, uint64_t l4_off, struct bpf_sock_tuple *tuple) {
+        if ((skb->data + ETH_HLEN + l4_off) > skb->data_end)
+                return -1;
 
+        struct {
+                uint16_t sport;
+                uint16_t dport;
+        } *tcphdr = (void *)(skb->data + ETH_HLEN + l4_off);
+
+        tuple->ipv4.sport = tcphdr->sport;
+        tuple->ipv4.dport = tcphdr->dport;
+
+        return 1;
 }
 
 static __always_inline int skb_extract_udp(struct __sk_buff *skb, uint32_t l4_off, struct bpf_sock_tuple *tuple) {
 
 }
 
-static __always_inline int skb_extract_ip4(struct __sk_buff *skb, struct bpf_sock_tuple *tuple) {
-        // if ((skb->data + ETH_HLEN) > skb->data_end)
-        //         return -1;
-        struct iphdr *ip4 = (struct iphdr *)(uint64_t)(skb->data + ETH_HLEN);
+static __always_inline int skb_extract_ip4(void *data, void *data_end, struct bpf_sock_tuple *tuple) {
+        if ((data + sizeof(struct ethhdr) + sizeof(struct iphdr)) > data_end)
+                return -1;
+        struct iphdr *ip4 = data + sizeof(struct ethhdr);
+
+        uint64_t l4_off = ETH_HLEN + IPV4_IHL_BYTES(ip4);
+
+        tuple->ipv4.saddr = ip4->saddr;
+        tuple->ipv4.daddr = ip4->daddr;
+
+        switch (ip4->protocol) {
+                case IPPROTO_TCP:
+                        // return skb_extract_tcp(skb, l4_off, tuple);
+                case IPPROTO_UDP:
+                        // return skb_extract_udp(skb, l4_off, tuple);
+                default:
+                        return -1;
+        }
 }
 
-static __always_inline int skb_extract_ip6(struct __sk_buff *skb, struct bpf_sock_tuple *tuple) {
+static __always_inline int skb_extract_ip6(void *data, void *data_end, struct bpf_sock_tuple *tuple) {
 
 }
 
 static __always_inline int skb_extract_tuple(struct __sk_buff *skb, struct bpf_sock_tuple *tuple)
 {
-        struct ethhdr *eth = (struct ethhdr *)(uint64_t)skb->data;
+        void *data_end = (void *)(uint64_t)(skb->data_end);
+        void *data = (void *)(uint64_t)(skb->data);
+        if ((data + sizeof(struct ethhdr)) > data_end)
+                return -1;
+        struct ethhdr *eth = data;
         switch (eth->h_proto) {
                 case ETH_P_IP:
-                        skb_extract_ip4(skb, tuple);
-                        break;
+                        return skb_extract_ip4(data, data_end, tuple);
                 case ETH_P_IPV6:
-                        skb_extract_ip6(skb, tuple);
-                        break;
+                        return skb_extract_ip6(data, data_end, tuple);
                 default:
                         return -1;
         }
@@ -60,6 +93,18 @@ static __always_inline int skb_extract_tuple(struct __sk_buff *skb, struct bpf_s
 SEC("tc")
 int tc_ingress_socket_redirect(struct __sk_buff *skb) {
         bpf_skb_pull_data(skb, 0);
+
+        struct bpf_sock_tuple tuple = {0};
+
+        if (skb_extract_tuple(skb, &tuple) == -1)
+                return TC_ACT_OK;
+
+        uint64_t *sock = bpf_map_lookup_elem(&accepted_sockets, &tuple);
+        if (!sock)
+                return TC_ACT_OK;
+
+        if (!bpf_sk_redirect_map(skb, &accepted_sockets, (__u32)(uint64_t)&tuple, 0))
+                return TC_ACT_SHOT;
 
         return TC_ACT_OK;
 };
